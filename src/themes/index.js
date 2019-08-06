f55acbbe8b5539cfc5be9b6a2020b393b17547ae
rkomiyama <ryukomiyama@gmail.com>
2019-08-06 05:30:18 -0400

    Adds pink colors

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,13 +1,15 @@
-import light from './light'
-import dark from './dark'
-import red from './red'
-import purple from './purple'
-import green from './green'
+import light from "./light";
+import dark from "./dark";
+import red from "./red";
+import pink from "./pink";
+import purple from "./purple";
+import green from "./green";
 
 export default {
   light,
   dark,
   red,
+  pink,
   purple,
   green
-}
+};
